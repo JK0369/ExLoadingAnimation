31daa9063c19e49fe456bbe358de8e2e3b227b87
jake <palatable7@naver.com>
2023-10-27 20:29:31 +0900

    Implement

---
+++ ExSetContentOffsetCompletion/ExSetContentOffsetCompletion/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ExSetContentOffsetCompletion
+//
+//  Created by 김종권 on 2023/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private let tableView = {
+        let view = UITableView()
+        view.allowsSelection = false
+        view.backgroundColor = .clear
+        view.separatorStyle = .none
+        view.bounces = true
+        view.showsVerticalScrollIndicator = true
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.estimatedRowHeight = 34
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let dataSource = (0...100).map(String.init)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+        ])
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dataSource.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+}
