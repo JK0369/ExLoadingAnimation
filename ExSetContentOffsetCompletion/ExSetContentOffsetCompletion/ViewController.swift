3248fdaa3c1eecc3e740203e5c4cf0d28e8477cf
jake <palatable7@naver.com>
2023-10-27 20:47:00 +0900

    Imeplement

--- ExSetContentOffsetCompletion/ExSetContentOffsetCompletion/ViewController.swift
+++ ExSetContentOffsetCompletion/ExSetContentOffsetCompletion/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let button = {
+        let button = UIButton()
+        button.setTitle("scrollToBottom", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.setTitleColor(.blue, for: .highlighted)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     private let tableView = {
         let view = UITableView()
         view.allowsSelection = false
@@ -26,14 +34,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        button.addAction(
+            UIAction { [weak self] _ in
+                self?.tableView.scrollToBottom(completion: {
+                    print("finish!")
+                })
+            }, for: .touchUpInside
+        )
         tableView.dataSource = self
+        
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
         
         view.addSubview(tableView)
         NSLayoutConstraint.activate([
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 16),
         ])
     }
 }
@@ -46,5 +67,25 @@ extension ViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = dataSource[indexPath.row]
         return cell
+    }
+}
+
+extension UIScrollView {
+    func scrollToBottom(completion: (() -> ())? = nil) {
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height + contentInset.bottom)
+        if(bottomOffset.y > 0) {
+            setContentOffset(offset: bottomOffset, animated: true, completion: completion)
+        }
+    }
+    
+    func setContentOffset(offset: CGPoint, animated: Bool, completion: (() -> ())? = nil) {
+        let keypath = "contentOffsetAnimationDuration"
+        guard let duration = value(forKey: keypath) as? Double else { return }
+        
+        setContentOffset(offset, animated: true)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+            completion?()
+        }
     }
 }
